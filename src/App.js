2a4fedfbd13e60b768fdfdc1b45c54d5fe06a743
Elmurat <example@gmail.com>
2022-02-15 18:55:22 +0600

    почти все сделано

--- src/App.js
+++ src/App.js
@@ -1,69 +1,19 @@
+import React from 'react';
 
-import React from 'react'
-import {Route,BrowserRouter as Router} from 'react-router-dom';
-import {useDispatch} from 'react-redux';
-import axios from 'axios';
-import {Header} from './components'
-import {Home,Cart} from './components/pages'
-import {setPizzas } from './redux/actions/pizzas'
+import { Header } from './components';
+import { Home, Cart } from './pages';
+import { Route } from 'react-router-dom';
 
-
-import '../src/scss/app.scss';
-
-
-
-
-
-function  App  (){
-  const dispatch = useDispatch()
- 
-
-  React.useEffect(()=>{
-        axios.get('http://localhost:3000/db.json').then(({data}) =>{
-        dispatch(setPizzas(data.pizzas))
-      })
-    })
-
-
-  return(
-    <Router>
-       <div className="wrapper">
-       <Header />
-         <div className="content">
-           <Route path = "/" component={Home} exact />
-           <Route path = "/cart"  component ={Cart} exact />
-         </div>
-       </div>
-       </Router>
-  )
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Route path="/" component={Home} exact />
+        <Route path="/cart" component={Cart} exact />
+      </div>
+    </div>
+  );
 }
 
-export default App
-
-// class App extends React.Component {
-
-//   componentWillMount() {
-//         axios.get('http://localhost:3000/db.json').then(({data}) =>{
-//             this.props.setPizzas(data.pizzas);
-//         })
-//   }
-
-
-//   render() {
-
-//   }
-// }
-
-// const mapStateToProps = (state)=>{
-//   return {
-//     items: state.pizzas.items
-//   }
-// }
-
-// const mapDispatchToProps = (dispatch)=>{
-//   return{
-//     setPizzas: (items) => dispatch(setPizzasAction(items))
-//   }
-// }
-
-// export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default App;
--- src/components/pages/Cart.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Cart = () => {
-  return <div>
-      <h1>привет</h1>
-  </div>;
-};
-
-export default Cart;
